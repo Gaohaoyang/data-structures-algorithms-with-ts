ee4751ef931c881c190cb019d241976d0fabe05a
chuan shi(haoyang gao) <ghy118432@alibaba-inc.com>
2018-12-18 17:29:28 +0800

    Lists done

---
+++ src/Lists.ts
@@ -0,0 +1,138 @@
+// 列表
+class List {
+  constructor() {
+    this.dataStore = []
+  }
+
+  private dataStore: any[]
+
+  public listSize: number = 0
+
+  public pos: number = 0
+
+  /**
+   * append
+   */
+  public append(element: any): void {
+    this.dataStore[this.listSize] = element
+    this.listSize += 1
+  }
+
+  /**
+   * remove
+   */
+  public remove(element: any): boolean {
+    const index: number = this.find(element)
+    if (index === -1) {
+      return false
+    }
+    this.dataStore.splice(index, 1)
+    this.listSize -= 1
+    return true
+  }
+
+  /**
+   * find
+   */
+  public find(element: any): number {
+    return this.dataStore.indexOf(element)
+  }
+
+  /**
+   * length
+   */
+  public length(): number {
+    return this.listSize
+  }
+
+  /**
+   * toString
+   */
+  public toString(): string {
+    return this.dataStore.join(',')
+  }
+
+  /**
+   * insert
+   */
+  public insert(element: any, afterElement: any): boolean {
+    const insertPos = this.find(afterElement)
+    if (insertPos === -1) {
+      return false
+    }
+    this.dataStore.splice(insertPos + 1, 0, element)
+    this.listSize += 1
+    return true
+  }
+
+  /**
+   * clear
+   */
+  public clear(): void {
+    this.dataStore = []
+    this.listSize = 0
+    this.pos = this.listSize
+  }
+
+  /**
+   * contains
+   */
+  public contains(element: any): boolean {
+    return this.find(element) !== -1
+  }
+
+  /**
+   * front
+   */
+  public front(): void {
+    this.pos = 0
+  }
+
+  /**
+   * end
+   */
+  public end(): void {
+    this.pos = this.listSize - 1
+  }
+
+  /**
+   * prev
+   */
+  public prev(): void {
+    if (this.pos > 0) {
+      this.pos -= 1
+    }
+  }
+
+  /**
+   * next
+   */
+  public next(): void {
+    if (this.pos < this.listSize - 1) {
+      this.pos += 1
+    }
+  }
+
+  /**
+   * currPos
+   */
+  public currPos(): number {
+    return this.pos
+  }
+
+  /**
+   * moveTo
+   */
+  public moveTo(postion: number): void {
+    this.pos = postion
+  }
+
+  /**
+   * getElement
+   */
+  public getElement() {
+    return this.dataStore[this.pos]
+  }
+}
+
+export default List
---
+++ test/Lists.test.js
@@ -0,0 +1,96 @@
+import List from '../src/Lists'
+
+describe('Lists', () => {
+  const list = new List()
+
+  test('append', () => {
+    list.append(123)
+    expect(list.dataStore).toEqual([123])
+    list.append('cs')
+    expect(list.dataStore).toEqual([123, 'cs'])
+  })
+
+  test('listSize', () => {
+    expect(list.listSize).toBe(2)
+    list.append('cs2')
+    expect(list.listSize).toBe(3)
+  })
+
+  test('find', () => {
+    expect(list.find('cs')).toBe(1)
+    expect(list.find('ghy')).toBe(-1)
+  })
+
+  test('remove', () => {
+    const originListSize = list.listSize
+    const res = list.remove('cs')
+    expect(res).toBe(true)
+    expect(list.listSize).toBe(originListSize - 1)
+    expect(list.dataStore).toEqual([123, 'cs2'])
+
+    const originListSize2 = list.listSize
+    const res2 = list.remove('ghy')
+    expect(res2).toBe(false)
+    expect(list.listSize).toBe(originListSize2)
+    expect(list.dataStore).toEqual([123, 'cs2'])
+  })
+
+  test('length', () => {
+    expect(list.length()).toBe(2)
+  })
+
+  test('toString', () => {
+    expect(list.toString()).toBe('123,cs2')
+  })
+
+  test('insert', () => {
+    const originListSize = list.listSize
+    const insertRes = list.insert('gaohaoyang', 123)
+    expect(insertRes).toBe(true)
+    expect(list.listSize).toBe(originListSize + 1)
+    expect(list.toString()).toBe('123,gaohaoyang,cs2')
+
+    const originListSize2 = list.listSize
+    const insertRes2 = list.insert('age', 1234)
+    expect(insertRes2).toBe(false)
+    expect(list.listSize).toBe(originListSize2)
+    expect(list.toString()).toBe('123,gaohaoyang,cs2')
+  })
+
+  test('clear', () => {
+    list.clear()
+    expect(list.listSize).toBe(0)
+    expect(list.toString()).toBe('')
+  })
+
+  test('contains', () => {
+    expect(list.contains('123')).toBe(false)
+    list.append(1)
+    expect(list.contains(1)).toBe(true)
+  })
+
+  test('iterator ', () => {
+    list.append('123')
+    list.append('765')
+    list.append('ghy')
+    list.append('cs')
+    list.front()
+    expect(list.getElement()).toBe(1)
+    list.next()
+    expect(list.getElement()).toBe('123')
+    list.prev()
+    expect(list.getElement()).toBe(1)
+    list.end()
+    expect(list.getElement()).toBe('cs')
+    list.moveTo(2)
+    expect(list.getElement()).toBe('765')
+    expect(list.currPos()).toBe(2)
+
+    list.front()
+    list.prev()
+    expect(list.currPos()).toBe(0)
+    list.end()
+    list.next()
+    expect(list.currPos()).toBe(list.listSize - 1)
+  })
+})
