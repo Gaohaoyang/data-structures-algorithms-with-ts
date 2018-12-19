a5a2942a73dd5a3c48c64defd2c497da89dbc3e7
chuan shi(haoyang gao) <ghy118432@alibaba-inc.com>
2018-12-19 16:01:35 +0800

    checkBracket

--- test/Stacks.test.js
+++ test/Stacks.test.js
@@ -32,3 +32,40 @@ describe('Stack', () => {
     expect(stack.length()).toBe(0)
   })
 })
+
+// 判断算式括号是否匹配
+// 遇到左括号入栈，遇到右括号出栈，到最后判断栈是否为空，为空则说明左右括号是匹配的
+const checkBracket = (ex) => {
+  const stack = new Stack()
+  for (let i = 0; i < ex.length; i += 1) {
+    if (ex[i] === '(') {
+      stack.push(i)
+    }
+    if (ex[i] === ')') {
+      if (stack.top > 0) {
+        stack.pop()
+      } else {
+        stack.push(i)
+      }
+    }
+  }
+  if (stack.top > 0) {
+    const pos = []
+    for (let i = 0; i < stack.top; i += 1) {
+      pos.push(stack.dataStore[i])
+    }
+    return pos.join(',')
+  }
+  return '-1'
+}
+
+test('checkBracket ', () => {
+  const expression = '2.3+23/12+(3.14159*0.24'
+  expect(checkBracket(expression)).toBe('10')
+  const expression2 = '2.3+23/12+(3.14159)*0.24'
+  expect(checkBracket(expression2)).toBe('-1')
+  expect(checkBracket('(')).toBe('0')
+  expect(checkBracket(')')).toBe('0')
+  expect(checkBracket('()()((')).toBe('4,5')
+  expect(checkBracket(')()(')).toBe('0,3')
+})
