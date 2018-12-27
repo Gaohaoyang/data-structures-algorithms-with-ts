68d3a1fa960a77558231feb1f954874b6649861c
chuan shi(haoyang gao) <ghy118432@alibaba-inc.com>
2018-12-27 23:00:53 +0800

    c

---
+++ src/Queues.ts
@@ -0,0 +1,54 @@
+class Queue {
+  constructor() {
+    this.dataStore = []
+  }
+
+  private dataStore: any[]
+
+  /**
+   * enqueue
+   */
+  public enqueue(e: any): void {
+    this.dataStore.push(e)
+  }
+
+  /**
+   * dequeue
+   */
+  public dequeue() {
+    this.dataStore.shift()
+  }
+
+  /**
+   * front
+   */
+  public front() {
+    return this.dataStore[0]
+  }
+
+  /**
+   * back
+   */
+  public back() {
+    return this.dataStore[this.dataStore.length - 1]
+  }
+
+  /**
+   * empty
+   */
+  public isEmpty(): boolean {
+    if (this.dataStore.length === 0) {
+      return true
+    }
+    return false
+  }
+
+  /**
+   * toString
+   */
+  public toString() {
+    return this.dataStore.join(',')
+  }
+}
+
+export default Queue
---
+++ test/Queues.test.js
@@ -0,0 +1,19 @@
+import Queue from '../src/Queues'
+
+describe('Queue', () => {
+  const q = new Queue()
+  q.enqueue(1123)
+  q.enqueue('chuanshi')
+  q.enqueue('666')
+  test('queue', () => {
+    expect(q.toString()).toBe('1123,chuanshi,666')
+    q.dequeue()
+    expect(q.toString()).toBe('chuanshi,666')
+    expect(q.front()).toBe('chuanshi')
+    expect(q.back()).toBe('666')
+    expect(q.isEmpty()).toBe(false)
+    q.dequeue()
+    q.dequeue()
+    expect(q.isEmpty()).toBe(true)
+  })
+})
